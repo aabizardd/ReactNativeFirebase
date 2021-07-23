a6685b088ea3d5847b5a498a0101b270de86f2ec
aabizardd <abizard@student.telkomuniversity.ac.id>
2021-07-23 11:19:34 +0700

    configurasi firebase

---
+++ src/config/FIREBASE/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyDM4lu4UMXj04DjERe_bpO1vHaKwa-6nx4',
+  authDomain: 'crud-react-native-a539b.firebaseapp.com',
+  projectId: 'crud-react-native-a539b',
+  storageBucket: 'crud-react-native-a539b.appspot.com',
+  messagingSenderId: '546321327022',
+  appId: '1:546321327022:web:33339fe81a76cf457cde3f',
+});
+
+const FIERBASE = firebase;
+
+export default FIERBASE;
